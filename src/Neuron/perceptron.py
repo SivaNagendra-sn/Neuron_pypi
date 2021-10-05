393b7514af83812d1daa700c8ec569478dc79fb7
SivaNagendra-sn <sivanagendra.paruchuri@gmail.com>
2021-10-05 17:41:33 +0530

    Workflow added

--- src/Neuron/perceptron.py
+++ src/Neuron/perceptron.py
@@ -9,7 +9,7 @@ from tqdm import tqdm
 
 class Perceptron:
   def __init__(self, eta, epochs):
-    self.weights = np.random.randn(3) * 1e-4 # SMALL WEIGHT INIT
+    self.weights = np.random.randn(3) * 1e-4 # Small weight initialization
     logging.info(f"initial weights before training: \n{self.weights}")
     self.eta = eta # LEARNING RATE
     self.epochs = epochs 
